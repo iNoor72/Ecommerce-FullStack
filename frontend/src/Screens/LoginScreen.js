3cf8c2aaa886b81c793c0315d48287b6f930fc87
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-09-05 20:11:07 +0200

    Added the styling for the login/register screens

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -4,8 +4,8 @@ function LoginScreen(props) {
     return <div class="log-form">
     <h2 className="LoginHeader">Login to your account</h2>
     <form method="post">
-        <input type="text" title="username" name="username" placeholder="username" className="usernameText"/>
-        <input type="password" title="password" name="password" placeholder="password" className="passwordText"/>
+        <input type="text" title="username" name="username" placeholder="Username" className="usernameText"/>
+        <input type="password" title="password" name="password" placeholder="Password" className="passwordText"/>
         <button type="submit" class="btn">Login</button>
     </form>
      <div id="demo"></div>
--- frontend/src/Screens/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function RegisterScreen(props) {
     return <div class="log-form">
-    <h2>Register for a new account</h2>
+    <h2 className="RegisterHeader">Register for a new account</h2>
     <form method="post" id="form">
-        <input type="text" name="username" id="username" title="username" placeholder="Username" required />
-        <input type="text" name="email" title="email" type="email" placeholder="Email" required />
-        <input type="password" name="password" title="password" placeholder="Password" required />
-        <button type="submit" name="submit" class="btn">register</button>
-        <a href="index.html" class="cancel-btn" ><button type="button" class="btn">Cancel</button></a>
+        <input type="text" name="username" id="username" title="username" placeholder="Username" required className="registerUsernameText"/>
+        <input type="text" name="email" title="email" type="email" placeholder="Email" required className="registerEmailText"/>
+        <input type="password" name="password" title="password" placeholder="Password" required className="registerpasswordText"/>
+        <button type="submit" name="submit" class="btn">Register</button>
+        <button type="button" class="Cancelbtn">Cancel</button>
     </form>
     <div id="demo"></div>
 </div>
