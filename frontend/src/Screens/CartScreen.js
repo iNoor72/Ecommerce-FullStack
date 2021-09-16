c8e6681438aafee26fba76fdc97d690986bcbd8e
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-09-16 17:11:14 +0200

    Added the design and styling for the cart screen

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -25,68 +25,68 @@ function CartScreen(props) {
   }
 
   return <div className="cart">
-    <div className="cart-list">
-      <ul className="cart-list-container">
-        <li>
-          <h3>
-            Shopping Cart
-          </h3>
+  <div className="cart-list">
+    <ul className="cart-list-container">
+      <li>
+        <h3>
+          Shopping Cart
+        </h3>
+        <div>
+          Price
+        </div>
+      </li>
+      {
+        cartItems.length === 0 ?
           <div>
-            Price
-          </div>
-        </li>
-        {
-          cartItems.length === 0 ?
-            <div>
-              Cart is empty
-          </div>
-            :
-            cartItems.map(item =>
-              <li>
-                <div className="cart-image">
-                  <img src={item.image} alt="product" />
-                </div>
-                <div className="cart-name">
-                  <div>
-                    <Link to={"/product/" + item.product}>
-                      {item.name}
-                    </Link>
+            Cart is empty
+        </div>
+          :
+          cartItems.map(item =>
+            <li>
+              <div className="cart-image">
+                <img src={item.image} alt="product" />
+              </div>
+              <div className="cart-name">
+                <div>
+                  <Link to={"/product/" + item.product}>
+                    {item.name}
+                  </Link>
 
-                  </div>
-                  <div>
-                    Qty:
-                  <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
-                      {[...Array(item.countInStock).keys()].map(x =>
-                        <option key={x + 1} value={x + 1}>{x + 1}</option>
-                      )}
-                    </select>
-                    <button type="button" className="button" onClick={() => removeFromCartHandler(item.product)} >
-                      Delete
-                    </button>
-                  </div>
                 </div>
-                <div className="cart-price">
-                  ${item.price}
+                <div>
+                  Qty:
+                <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
+                    {[...Array(item.countInStock).keys()].map(x =>
+                      <option key={x + 1} value={x + 1}>{x + 1}</option>
+                    )}
+                  </select>
+                  <button type="button" className="cart-delete-button" onClick={() => removeFromCartHandler(item.product)} >
+                    Delete
+                  </button>
                 </div>
-              </li>
-            )
-        }
-      </ul>
-
-    </div>
-    <div className="cart-action">
-      <h3>
-        Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
-        :
-         $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-      </h3>
-      <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
-        Proceed to Checkout
-      </button>
-
-    </div>
+              </div>
+              <div className="cart-price">
+                ${item.price}
+              </div>
+            </li>
+          )
+      }
+    </ul>
 
   </div>
+  <div className="cart-action">
+    <h3>
+      Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
+      :
+       $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+    </h3>
+    <button onClick={checkoutHandler} className="button-primary-full-width" disabled={cartItems.length === 0}>
+      Proceed to Checkout
+    </button>
+
+  </div>
+
+</div>
 }
 
 export default CartScreen;
