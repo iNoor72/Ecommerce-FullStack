318e09580cd361edf3530956072a44ad82de2ae6
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-09-17 17:39:53 +0200

    Added the checkout screen design and styling

--- frontend/src/Screens/CheckoutScreen.js
+++ frontend/src/Screens/CheckoutScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function CheckoutScreen(props) {
-    return <div>Checkout page</div>
+    return <div className="checkout-container">
+
+        <h3>
+            Order was made successfully. Your Order number is #000000.
+            
+        </h3>
+        <p>Thank you for using Estabena.</p>
+
+    </div>
 }
 
 export default CheckoutScreen;
