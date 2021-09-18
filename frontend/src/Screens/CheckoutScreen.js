400cd44a9e693747d80d6cbc82a0361766591761
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-09-18 19:03:41 +0200

    Fixed a small problem with the order number

--- frontend/src/Screens/CheckoutScreen.js
+++ frontend/src/Screens/CheckoutScreen.js
@@ -4,7 +4,7 @@ function CheckoutScreen(props) {
     return <div className="checkout-container">
 
         <h3>
-            Order was made successfully. Your Order number is #000000.
+            Order was made successfully. Your Order number is #{Math.floor((Math.random() * 9999999)) + 1}.
             
         </h3>
         <p>Thank you for using Estabena.</p>
